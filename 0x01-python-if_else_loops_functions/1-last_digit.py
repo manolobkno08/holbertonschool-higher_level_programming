1452d2b2ed083c662f26b2e8687d2388d1cbe2b5
manolobkno08 <manologm10@hotmail.com>
2021-09-07 12:45:42 -0500

    fixed 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if num >= 0:
 else:
     last_d = num % -10
 if last_d > 5:
-    print("{} {} is {} and is greater than 5".format(str1, num, last_d))
+    print("{} {:d} is {} and is greater than 5".format(str1, num, last_d))
 elif last_d == 0:
-    print("{} {} is {} and is 0".format(str1, num, last_d))
+    print("{} {:d} is {} and is 0".format(str1, num, last_d))
 elif last_d < 6 and last_d != 0:
     print("{} {} is {} and is less than 6 and not 0".format(str1, num, last_d))
