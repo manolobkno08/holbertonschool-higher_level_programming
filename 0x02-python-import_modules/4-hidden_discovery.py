42d13d7bf17ac98d9d8593bf14638991de1c200a
manolobkno08 <manologm10@hotmail.com>
2021-09-09 22:10:16 -0500

    4 fixed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import hidden_4
+import sys
+import hidden_4
+
+if __name__ == "__main__":
     for i in dir(hidden_4):
-        if i[:2] != "--":
-            print(i)
+        if i[0] != '_':
+            print("{}".format(i))
