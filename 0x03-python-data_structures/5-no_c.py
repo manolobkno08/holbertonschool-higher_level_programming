346d65793f7949ec4b5b33a6e1a6ad6fd714bf15
manolobkno08 <manologm10@hotmail.com>
2021-09-13 14:22:58 -0500

    add task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    str1 = ""
     if my_string is None:
         return None
     else:
         for i in my_string:
             if i == "c" or i == "C":
                 continue
-            print("".format(i), end="")
+            str1 += i
+        return str1
