26c773d7d0a815d8dca778e63e51a7f22e6e9083
manolobkno08 <manologm10@hotmail.com>
2021-09-13 13:49:51 -0500

    task4 fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/puthon3
 def new_in_list(my_list, idx, element):
-    cpstr = my_list.copy()
-    if idx < 0:
+    cpstr = my_list[:]
+    if idx < 0 or idx > len(my_list):
         return cpstr
-    elif idx > len(my_list):
-        return cpstr
-    else:
-        for i in range(len(cpstr)):
-            if i == idx:
-                cpstr[i] = element
-                return cpstr
+    cpstr[idx] = element
+    return cpstr
