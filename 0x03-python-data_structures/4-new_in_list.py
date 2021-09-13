52448e68fcbb588c3b41280c8a8586ca54256474
manolobkno08 <manologm10@hotmail.com>
2021-09-13 13:59:37 -0500

    task4 fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cpstr = my_list[:]
+    cpstr = my_list
     if idx < 0 or idx > len(my_list):
         return cpstr
+    cpstr = my_list[:]
     cpstr[idx] = element
     return cpstr
