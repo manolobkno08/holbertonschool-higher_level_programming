e9cfe8d3ab6caa8c82b78a0f11813e1642ccf922
manolobkno08 <manologm10@hotmail.com>
2021-09-13 16:09:09 -0500

    add task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for column in row:
+            print("{:d}".format(column), end="")
+            if column != row[len(row) - 1]:
+                print("", end=" ")
+        print("")
