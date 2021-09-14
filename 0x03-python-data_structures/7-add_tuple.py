2ce28f1d73631bd37e616c91bcc010dc27f1fd0d
manolobkno08 <manologm10@hotmail.com>
2021-09-13 19:23:12 -0500

    fixed task7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,5 +10,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = (0, 0)
         else:
             tuple_b = tuple_b[0], 0
-    tup = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    tup = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return tup
