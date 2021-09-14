1f1961eb85345158fd4190bbb4517487a3399be6
manolobkno08 <manologm10@hotmail.com>
2021-09-13 19:19:52 -0500

    fixed task7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a is None or tuple_b is None:
-        return None
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = (0, 0)
