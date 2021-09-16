7959c8d9f20419d7050aa1863f531da845546196
manolobkno08 <manologm10@hotmail.com>
2021-09-15 23:41:16 -0500

    add task13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    return sum([mult(x[0], x[1]) for x in my_list])/sum(x[1] for x in my_list)
+        
+def mult(a, b):
+    return (a * b)
