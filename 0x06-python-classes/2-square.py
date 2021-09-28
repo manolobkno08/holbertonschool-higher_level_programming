af68020c89024d072dd985a7ac868a78bfb7627c
manolobkno08 <manologm10@hotmail.com>
2021-09-28 18:40:05 -0500

    add task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square is a class"""
+
+
+class Square():
+    """Return private attribute"""
+
+    def __init__(self, size=0):
+        """size must be an integer, otherwise raise a TypeError"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            """if size is less than 0, raise a ValueError"""
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                """Private instance attribute: size"""
+                self.__size = size
