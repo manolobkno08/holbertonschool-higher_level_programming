6d80e6b47f39f008ab0a880011fd7d69acdb2a7f
manolobkno08 <manologm10@hotmail.com>
2021-09-30 00:28:31 -0500

    task4 fixed

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
     """Return a repr"""
 
     def __repr__(self):
-        return (f"Rectangle({self.__width},{self.__height})")
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
