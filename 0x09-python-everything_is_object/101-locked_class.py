f7b0e8609f95539c6486fca15f95499079bf7484
manolobkno08 <manologm10@hotmail.com>
2021-09-30 23:30:37 -0500

    add task31

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 class LockedClass:
-    """Onlyreate the instance attribute 'first_name'"""
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
     __slots__ = ['first_name']
