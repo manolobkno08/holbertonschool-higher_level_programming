94ac7c6e205d294742ed5b6e3b35d915b643c866
manolobkno08 <manologm10@hotmail.com>
2021-10-08 14:54:02 -0500

    add task5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,9 +8,12 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_signed_ints_and_floats(self):
-        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([3]), 3)
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([4, 2, 4, 1]), 4)
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
         self.assertEqual(max_integer([1, 2, 3, -4]), 3)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def test_None(self):
         self.assertIsNone(max_integer([]), None)
