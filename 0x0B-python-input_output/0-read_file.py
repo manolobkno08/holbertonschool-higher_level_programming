8405a84a3314f95eae790b76c347f6e4dc4372b8
manolobkno08 <manologm10@hotmail.com>
2021-10-12 17:10:19 -0500

    task1 fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Function that read a text file
 def read_file(filename=""):
     """Read specified file"""
     with open(filename, "r", encoding='utf-8') as f:
-        print(f.read())
+        print(f.read(), end="")
