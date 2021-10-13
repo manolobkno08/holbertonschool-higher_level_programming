233fa715da1b4c5546a57763be9a2a857fd2cc69
manolobkno08 <manologm10@hotmail.com>
2021-10-12 23:55:08 -0500

    add task10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Class that defines a Student
+"""
+
+
+class Student():
+    """Define Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for row in attrs:
+                if row in self.__dict__.keys():
+                    new_dict[row] = self.__dict__[row]
+            return new_dict
+
+
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
