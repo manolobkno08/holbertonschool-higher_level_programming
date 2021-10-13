02d1f864ca6eca9cb90ed0558140099927853b0c
manolobkno08 <manologm10@hotmail.com>
2021-10-13 17:08:05 -0500

    add task11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Class that defines a Student
+"""
+
+
+class Student():
+    """Define Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for row in attrs:
+                if row in self.__dict__.keys():
+                    new_dict[row] = self.__dict__[row]
+            return new_dict
+
+    def reload_from_json(self, json):
+        if json is not None:
+            for attr, value in json.items():
+                setattr(self, attr, value)
