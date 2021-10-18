62b889272d35fb7e4fd39ca36829475942cdd0ea
manolobkno08 <manologm10@hotmail.com>
2021-10-18 16:52:36 -0500

    create new comments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Create inherents class of Base"""
+"""
+Create inherents class of Base
+"""
 
 from .rectangle import Rectangle
 
@@ -8,22 +10,27 @@ class Square(Rectangle):
     """New Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initiallize attributes"""
         super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
     def size(self):
+        """Size property"""
         return (self.width)
 
     @size.setter
     def size(self, size):
+        """Size Setter"""
         self.width = size
         self.height = size
 
     def __str__(self):
+        """String representation"""
         return ("[Square] ({}) {}/{} - {}". format(self.id, self.x, self.y, self.size))
 
     def update(self, *args, **kwargs):
+        """Updating data"""
         if len(args) != 0:
             for i, arg in enumerate(args):
                 if i == 0:
@@ -45,6 +52,7 @@ class Square(Rectangle):
                 self.y = kwargs['y']
 
     def to_dictionary(self):
+        """Create a dictionary"""
         dictionary = {}
         dictionary['id'] = self.id
         dictionary['x'] = self.x
