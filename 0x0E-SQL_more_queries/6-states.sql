0f814f0e3dea04f977fde2379bacb07e19bd7aab
manolobkno08 <manologm10@hotmail.com>
2021-11-17 18:40:46 -0500

    add task6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Create a table as unique value
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(256) NOT NULL
 );
