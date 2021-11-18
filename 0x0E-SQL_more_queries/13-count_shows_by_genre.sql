c1457c8d21d7c7ac578fe81d0f80c5e883221585
manolobkno08 <manologm10@hotmail.com>
2021-11-17 23:51:24 -0500

    add task13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- List Gender
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre.id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_genres
-JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id IS NULL
+INNER JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+ORDER BY number_shows DESC;
