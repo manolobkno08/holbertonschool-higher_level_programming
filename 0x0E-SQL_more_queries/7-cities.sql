a057ae6de5e7585d7be5a966f366e995aa5b27cf
manolobkno08 <manologm10@hotmail.com>
2021-11-17 20:00:34 -0500

    add task7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Create a table - foreign key
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
+	CONSTRAINT fk_id_states FOREIGN KEY (`state_id`) REFERENCES states (`id`)
+);
