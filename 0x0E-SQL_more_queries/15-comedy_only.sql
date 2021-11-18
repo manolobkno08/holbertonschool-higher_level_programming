b7f83ef4c8a0305a08964e2bd345b59cdd3cdac2
manolobkno08 <manologm10@hotmail.com>
2021-11-18 15:38:50 -0500

    add task15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- List Gender
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_genres
-ON tv_shows.genre_id = tv_genres.genre_id
+JOIN tv_genres_genres
+ON tv_shows.id = tv_shows.genres.genre.show_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY show_tv.title;
