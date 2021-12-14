6bfca9e558c155dea62cd1efd91e263437a8d827
manolobkno08 <manologm10@hotmail.com>
2021-12-13 22:28:40 -0500

    add task10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv;
+// console.log(args[2])
+
+const x = parseInt(args[2]);
+
+try {
+  const res = factorial(x);
+  console.log(res);
+} catch (error) {
+  console.error('1');
+}
+
+function factorial (x) {
+  if (x === 0) {
+    return 1;
+  }
+  return x * factorial(x - 1);
+}
