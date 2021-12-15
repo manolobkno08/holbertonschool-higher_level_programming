dd52a88f1be51da18befd044dd6cad03ddb7d30c
manolobkno08 <manologm10@hotmail.com>
2021-12-14 22:13:08 -0500

    add task6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const Square1 = require('./5-square');
+
+module.exports = class Square extends Square1 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('X');
+        }
+        console.log('');
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('C');
+        }
+        console.log('');
+      }
+    }
+  }
+};
