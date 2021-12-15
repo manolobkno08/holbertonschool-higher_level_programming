8f5fed432af20643d8b1f364de73b70dbb40a532
manolobkno08 <manologm10@hotmail.com>
2021-12-15 15:49:59 -0500

    fixed task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,16 +7,16 @@ const args = process.argv;
 const myArray = [];
 
 if (args.length <= 3) {
-	console.log(0)
-} else if (args.length > 3){
-	args.forEach(function (element) {
-		const x = parseInt(element);
-		if (!isNaN(x)) {
-			myArray.push(element);
-		}
-	});
-	y = myArray.sort();
-	j = myArray.pop();
-	x = myArray[myArray.length - 1]
-	console.log(x);
+  console.log(0);
+} else if (args.length > 3) {
+  args.forEach(function (element) {
+    const x = parseInt(element);
+    if (!isNaN(x)) {
+      myArray.push(element);
+    }
+  });
+  myArray.sort();
+  myArray.pop();
+  const x = myArray[myArray.length - 1];
+  console.log(parseInt(x));
 }
