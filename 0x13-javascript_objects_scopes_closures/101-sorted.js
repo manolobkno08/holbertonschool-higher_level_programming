560c4b96ac0a36e772e9e2ef5018b0251c5bccd7
manolobkno08 <manologm10@hotmail.com>
2021-12-15 20:54:33 -0500

    add task101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,10 +4,10 @@ const obj = require('./101-data').dict;
 
 const newDict = {};
 
-for (let key in obj) {
-	if (newDict[obj[key]] === undefined) {
-		newDict[obj[key]] = [];
-	}
-	newDict[obj[key]].push(key);
+for (const key in obj) {
+  if (newDict[obj[key]] === undefined) {
+    newDict[obj[key]] = [];
+  }
+  newDict[obj[key]].push(key);
 }
 console.log(newDict);
