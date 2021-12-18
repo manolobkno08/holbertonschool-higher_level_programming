55b8a41bcd73021451b239c15f99346880ac4b1a
manolobkno08 <manologm10@hotmail.com>
2021-12-18 13:24:07 -0500

    task11 fixed

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python3
-from sqlalchemy.orm import sessionmaker
+
+"""
+SQLAlchemy Statements
+
+"""
 
 from sys import argv
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+def connection():
+    """Connection to database"""
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                               format(
+                                   argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+    except Exception:
+        print("Can't connect to DB")
+        return 0
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    try:
-        new_state = State(name='Louisiana')
-        session.add(new_state)
-        session.commit()
-        print(new_state.id)
-    except:
-        pass
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
 
     session.close()
+
+
+if __name__ == "__main__":
+    connection()
