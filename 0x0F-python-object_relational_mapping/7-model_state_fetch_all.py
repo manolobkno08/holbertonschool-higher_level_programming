016708fa02c045bc9bbdfb780efb45da1a748fbb
manolobkno08 <manologm10@hotmail.com>
2021-12-18 12:33:23 -0500

    task 7 fixed

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-from sqlalchemy.orm import sessionmaker
 
 from sys import argv
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+def connection():
+    """SQLAlchemy Statements"""
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                               format(
+                                   argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+    except Exception:
+        print("Can't connect to DB")
+        return 0
 
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
+
+
+if __name__ == "__main__":
+    connection()
