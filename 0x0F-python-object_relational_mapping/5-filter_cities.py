3a6599be0d13ef8f7366fe94e88bc73bd0c0dcdc
manolobkno08 <manologm10@hotmail.com>
2021-12-18 11:39:09 -0500

    task5 fixed

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -20,12 +20,12 @@ def connection():
         SELECT cities.name
         FROM cities
         INNER JOIN states ON cities.state_id = states.id
-        WHERE states.name = '{state}';
+        WHERE states.name = '{state}'
+        ORDER BY cities.state_id ASC
         """.format(state=argv[4])
     )
 
     query_rows = cur.fetchall()
-    print(query_rows)
     print(", ".join(city[0] for city in query_rows))
 
     cur.close()
