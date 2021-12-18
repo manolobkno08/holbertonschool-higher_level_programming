7711456fd0bb2ce57e908b3d7213504c5cf10d95
manolobkno08 <manologm10@hotmail.com>
2021-12-18 00:47:20 -0500

    update 8-11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
         new_state = State(name='Louisiana')
         session.add(new_state)
         session.commit()
+        session.close()
         print(new_state.id)
     except:
         pass
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     try:
-        for state in session.query(State).order_by(State.id).all():
+        for state in session.query(State).all():
             if state.id == 1:
                 print("{}: {}".format(state.id, state.name))
     except:
