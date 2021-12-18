e49572ae37688dfecc3462799cf50248361892e1
manolobkno08 <manologm10@hotmail.com>
2021-12-18 11:21:36 -0500

    task3 fixed

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,13 +3,27 @@
 import MySQLdb
 from sys import argv
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                       passwd=argv[2], db=argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC;",
-            (argv[4],))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+
+def connection():
+    """
+    Simple Query Function
+    """
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                               passwd=argv[2], db=argv[3], charset="utf8")
+    except Exception:
+        print("Can't connect to DB")
+        return 0
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC;",
+                (argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    connection()
