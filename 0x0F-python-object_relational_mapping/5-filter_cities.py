4576266354367dd25b1fb8659afc0037a0457479
manolobkno08 <manologm10@hotmail.com>
2021-12-18 11:34:32 -0500

    task5 fixed

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,20 +3,33 @@
 import MySQLdb
 from sys import argv
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                       passwd=argv[2], db=argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute(
-    """
-    SELECT cities.name
-    FROM cities
-    INNER JOIN states ON cities.state_id = states.id
-    WHERE states.name = '{state}';
-    """.format(state=argv[4])
-)
-x = []
-query_rows = cur.fetchall()
-print(", ".join(city[0] for city in query_rows))
 
-cur.close()
-conn.close()
+def connection():
+    """
+    Simple Query Function
+    """
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                               passwd=argv[2], db=argv[3], charset="utf8")
+    except Exception:
+        print("Can't connect to DB")
+        return 0
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name = '{state}';
+        """.format(state=argv[4])
+    )
+
+    query_rows = cur.fetchall()
+    print(query_rows)
+    print(", ".join(city[0] for city in query_rows))
+
+    cur.close()
+    conn.close()
+
+
+connection()
