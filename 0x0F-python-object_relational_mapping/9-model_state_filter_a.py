d7f630868247d1f0cd27a2559bdf10de51232eb3
manolobkno08 <manologm10@hotmail.com>
2021-12-18 13:14:29 -0500

    task9 fixed

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
-from sqlalchemy.orm import sessionmaker
+
+"""
+SQLAlchemy Statements
+
+"""
 
 from sys import argv
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+def connection():
+    """Connection to database"""
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                               format(
+                                   argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+    except Exception:
+        print("Can't connect to DB")
+        return 0
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        for state in session.query(State).all():
-            if 'a' in state.name:
-                print("{}: {}".format(state.id, state.name))
-    except:
-        print("Nothing")
+
+    for state in session.query(State).all():
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
+
     session.close()
+
+
+if __name__ == "__main__":
+    connection()
