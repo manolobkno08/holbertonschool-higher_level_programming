f6597a4772b5eccbe52d77e717e7ceaead270346
manolobkno08 <manologm10@hotmail.com>
2021-12-18 11:27:41 -0500

    task 4 fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,20 +3,32 @@
 import MySQLdb
 from sys import argv
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                       passwd=argv[2], db=argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute(
-    """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    INNER JOIN states ON cities.state_id = states.id
-    ORDER BY id ASC;
-    """
-)
 
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+def connection():
+    """
+    Simple Query Function
+    """
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                               passwd=argv[2], db=argv[3], charset="utf8")
+    except Exception:
+        print("Can't connect to DB")
+        return 0
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        ORDER BY id ASC;
+        """
+    )
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+connection()
