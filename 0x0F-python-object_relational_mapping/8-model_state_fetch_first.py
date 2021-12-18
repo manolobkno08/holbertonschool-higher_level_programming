16439944a686fb2427fa0bda0c44acda2eb86bf4
manolobkno08 <manologm10@hotmail.com>
2021-12-18 12:41:35 -0500

    task8 fixed

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-from sqlalchemy.orm import sessionmaker
+
+"""
+SQLAlchemy Statements
+
+"""
 
 from sys import argv
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+def connection():
+    """Connection to database"""
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                               format(
+                                   argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+    except Exception:
+        print("Can't connect to DB")
+        return 0
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -18,6 +30,10 @@ if __name__ == "__main__":
         for state in session.query(State).all():
             if state.id == 1:
                 print("{}: {}".format(state.id, state.name))
-    except:
+    except Exception:
         print("Nothing")
     session.close()
+
+
+if __name__ == "__main__":
+    connection()
