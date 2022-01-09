f3177b46c50d45d8d82c8b23def64e7081a253b1
manolobkno08 <manologm10@hotmail.com>
2022-01-09 15:11:46 -0500

    fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Header variables - POST
-curl -s -d "email:test@gmail.com&subject:I will always be here for PLD" -X POST "$1"
+curl -d "email:test@gmail.com&subject:I will always be here for PLD" -X POST "$1"
