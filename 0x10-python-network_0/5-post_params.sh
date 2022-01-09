5dfebd566f310565df28913a31ac4a3f9fcba82a
manolobkno08 <manologm10@hotmail.com>
2022-01-09 15:22:45 -0500

    fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Header variables - POST
-curl -s -d "email:test@gmail.com&subject:I will always be here for PLD" -H "Content-Type: application/json" -X POST "$1"
+curl -sI -d "email:test@gmail.com&subject:I will always be here for PLD" -H "Content-Type: application/json" -X POST "$1"
