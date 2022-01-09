d5a3cf395f8945a23fc606773d77305bcf5a6b55
manolobkno08 <manologm10@hotmail.com>
2022-01-09 15:03:12 -0500

    fixed

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Header variable send by 98
-curl -s -H "email:98" -H "subject:I will always be here for PLD" -X POST "$1"
+curl -sH "X-School-User-Id:98" GET "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Header variables - POST
+curl -s -H "email:98" -H "subject:I will always be here for PLD" -X POST "$1"
