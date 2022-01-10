47239ae94abe0d07ddf668bf225c411c84e0067f
manolobkno08 <manologm10@hotmail.com>
2022-01-09 19:32:04 -0500

    add task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import urllib.request
+    import urllib.request as request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    with request.urlopen('https://intranet.hbtn.io/status') as res:
         x = res.read()
         print(f"Body response:")
         print(f"\t- type: {type(x)}")
