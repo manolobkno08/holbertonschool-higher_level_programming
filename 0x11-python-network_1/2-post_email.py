fb7ae67e7a277206926abe9354186b20bcf8c44f
manolobkno08 <manologm10@hotmail.com>
2022-01-11 17:49:51 -0500

    task2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,11 +4,11 @@ Display value from POST
 """
 if __name__ == "__main__":
     from urllib import request, parse
-    import sys
+    from sys import argv
 
-    x = {'email': sys.argv[2]}
+    url = argv[1]
+    x = {'email': argv[2]}
     email = parse.urlencode(x).encode('utf-8')
-    url = sys.argv[1]
     req = request.Request(url, email)
     with request.urlopen(req) as r:
-        print(r.read())
+        print(r.read().decode('utf-8'))
