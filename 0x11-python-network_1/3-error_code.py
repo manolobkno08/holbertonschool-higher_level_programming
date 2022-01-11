90ab347ee7445e9ecbab0b924edecd0f377a9481
manolobkno08 <manologm10@hotmail.com>
2022-01-11 18:04:02 -0500

    add task

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Display Error code
+"""
+if __name__ == "__main__":
+    from urllib import request, error
+    from sys import argv
+
+    try:
+        with request.urlopen(argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except error.URLError as e:
+        print(f'Error code: {e.code}')
