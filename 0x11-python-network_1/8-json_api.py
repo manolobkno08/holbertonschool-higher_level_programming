628c41dc868676e5a77504b233d64ed739768f3e
manolobkno08 <manologm10@hotmail.com>
2022-01-11 20:08:47 -0500

    add task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Response Header
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    URL = "http://0.0.0.0:5000/search_user"
+
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+
+    req = requests.post(URL, data=q).json()
+    try:
+        if len(req) == 0 or not req.get('id') or not req.get('name'):
+            print("No result")
+        else:
+            print("[{}] {}".format(req.get('id'), req.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
