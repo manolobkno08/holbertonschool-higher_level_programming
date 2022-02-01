defda521daf5a4d655c3ca5a496a2386c64cd49f
manolobkno08 <manologm10@hotmail.com>
2022-02-01 14:26:30 -0500

    task 0 fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,14 +4,9 @@ const args = process.argv.slice(2);
 const file = args[0];
 
 fs.readFile(file, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
-})
-
-// Write a script that reads and prints the content of a file.
-
-// The first argument is the file path
-// If an error occurred during the reading, print the error object
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
