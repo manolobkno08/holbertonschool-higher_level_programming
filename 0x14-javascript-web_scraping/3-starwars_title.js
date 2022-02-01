a080470624b506c6b2488b1d506f84979f8d6fdd
manolobkno08 <manologm10@hotmail.com>
2022-02-01 17:06:58 -0500

    tab lines fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,15 +4,15 @@ const request = require('request');
 const args = process.argv.slice(2);
 const episode = args[0];
 
-async function movieTitle() {
-	const url = 'https://swapi-api.hbtn.io/api/films/';
-	await request(url + episode, function (err, response, body) {
-		if (err) {
-			console.log(err);
-		}
-		const obj = JSON.parse(body);
-		console.log(obj.title);
-	});
+async function movieTitle () {
+  const url = 'https://swapi-api.hbtn.io/api/films/';
+  await request(url + episode, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    }
+    const obj = JSON.parse(body);
+    console.log(obj.title);
+  });
 }
 
 movieTitle();
