4aeaa7fb3ccd7b627b01f103c57d1be4b244f1b5
manolobkno08 <manologm10@hotmail.com>
2022-02-01 14:41:07 -0500

    semistandard fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 const args = process.argv.slice(2);
-const file_path = args[0];
+const filePath = args[0];
 const content = args[1];
 
-fs.writeFile(file_path, content, 'utf8', err => {
+fs.writeFile(filePath, content, 'utf8', err => {
   if (err) {
     console.error(err);
   }
